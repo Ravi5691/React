8554457595a92a7c8ad182d3b29f8ec7022dfb7f
Ravinder Singh Bisht <ravinderbisht5691@gmail.com>
2024-08-23 12:28:50 +0530

    third commit

--- 12_MegaBlog/src/App.jsx
+++ 12_MegaBlog/src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import { useState , useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import confg from'./envImport/confg'
+import authservice from "./appwrite/authservice"
+import {login , logout} from "./store/authSlice"
+import {Header , Footer} from './components'
 
 function App() {
   // console.log(process.env.REACT_APP_APPWRITE_URL);
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  const[loading , setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1> A blog app with appwrite .</h1>
-    </>
-  )
+  useEffect(() => {
+    authservice.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'> 
+    <div className='w-full block'>
+      <Header/>
+      <main> 
+        {/* <Outlet/>  */}
+      </main>
+      <Footer/>
+    </div>
+    </div>
+  ) : null
 }
 
 export default App
--- 12_MegaBlog/src/envImport/confg.js
+++ 12_MegaBlog/src/envImport/confg.js
@@ -1,5 +1,5 @@
 const confg = {
-    appwriteurl: String(import.meta.env.VITE_APPWRITE.URL),
+    appwriteurl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
